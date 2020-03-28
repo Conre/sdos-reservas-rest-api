2d4c16b1030a202cc2ed239fffe9d3079973a39e
jesus <jesus00diaz@gmail.com>
2020-03-28 19:40:08 +0100

    Configuración inicial del proyecto

---
+++ src/main/java/sdos/prueba/jesus/pruebajesus/PruebaJesusApplication.java
@@ -0,0 +1,13 @@
+package sdos.prueba.jesus.pruebajesus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaJesusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaJesusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sdos/prueba/jesus/pruebajesus/PruebaJesusApplicationTests.java
@@ -0,0 +1,13 @@
+package sdos.prueba.jesus.pruebajesus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaJesusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
